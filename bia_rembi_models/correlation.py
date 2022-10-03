e587731cf7ec766bbe4ae34b562e93693f315b5e
Matthew Hartley <matthewh@ebi.ac.uk>
2022-10-03 17:33:55 +0100

    Add ignore file

---
+++ bia_rembi_models/correlation.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+from . import FreeText
+
+
+class SpatialAndTemporalAlignment(FreeText):
+    """Method used to correlate images from different modalities"""
+
+    _template_label = "Spatial/Temporal Alignment"
+
+    _examples = [
+        "Manual overlay",
+        "Alignment algorithm"
+    ]
+
+
+class FiducialsUsed(FreeText):
+    """Features from correlated datasets used for colocalisation"""
+
+    _template_label = "Fiducials Used"
+
+
+class TransformationMatrix(FreeText):
+    """Correlation transforms"""
+
+    _template_label = "Transformation Matrix"
+    _template_hint = "Filename if present"
+
+
+class ImageCorrelation(BaseModel):
+    """How images from the same correlative study are linked"""
+
+    spatial_and_temporal_alignment: SpatialAndTemporalAlignment
+    fiducials_used: FiducialsUsed
+    transformation_matrix: TransformationMatrix
