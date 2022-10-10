9a8d0b091d4bbd673b9e1120ddd33d8b0b2f78ba
Matthew Hartley <matthewh@ebi.ac.uk>
2022-10-10 19:54:21 +0100

    Add generation of schema json and the generated schemas

---
+++ scripts/generate_schemas.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from bia_rembi_models.study import Study
+from bia_rembi_models.sample import Biosample
+from bia_rembi_models.specimen import Specimen
+from bia_rembi_models.acquisition import ImageAcquisition
+from bia_rembi_models.correlation import ImageCorrelation
+from bia_rembi_models.analysis import ImageAnalysis
+
+
+def main():
+
+    output_dirpath = Path("schemas")
+
+    objects_to_write = [
+        Study,
+        Biosample,
+        Specimen,
+        ImageAcquisition,
+        ImageCorrelation,
+        ImageAnalysis
+    ]
+
+    for obj in objects_to_write:
+        output_fpath = output_dirpath/f"{obj.__name__}.json"
+
+        with open(output_fpath, "w") as fh:
+            fh.write(obj.schema_json(indent=2))
+
+
+if __name__ == "__main__":
+    main()
